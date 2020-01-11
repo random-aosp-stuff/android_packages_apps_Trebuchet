3892800c272ba87ddf102dfc90ef29d0f08c74f6
Amir Zaidi <azaidi@live.nl>
2020-01-11 13:55:58 +0100

    Launcher3: Add Icon Pack Support from Shade Launcher
    @DennySPb: port to A11
    
    Squashed with:
    AospLauncher: Bypass adaptive icon wrapping with icon pack icons
    AospLauncher: Clear all icon overrides when changing global icon pack
    
    Change-Id: Id81da397d5ef33577d6f53f349080c49a61591ab
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -23,6 +23,8 @@ import static com.android.launcher3.states.RotationHelper.ALLOW_ROTATION_PREFERE
 import static com.android.launcher3.states.RotationHelper.getAllowRotationDefaultValue;
 import static com.android.launcher3.util.SecureSettingsObserver.newNotificationSettingsObserver;
 
+import com.android.launcher3.customization.IconDatabase;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.SharedPreferences;
@@ -31,6 +33,10 @@ import android.os.Handler;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.MenuItem;
+
+import com.android.launcher3.settings.preference.IconPackPrefSetter;
+import com.android.launcher3.settings.preference.ReloadingListPreference;
+import com.android.launcher3.util.AppReloader;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
@@ -80,9 +86,14 @@ public class SettingsActivity extends FragmentActivity
     private static final String KEY_MINUS_ONE = "pref_enable_minus_one";
     private static final String SEARCH_PACKAGE = "com.google.android.googlequicksearchbox";
 
+    private static final String KEY_ICON_PACK = "pref_icon_pack";
+
+    private static Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = getApplicationContext();
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -113,6 +124,10 @@ public class SettingsActivity extends FragmentActivity
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public interface OnResumePreferenceCallback {
+        void onResume();
     }
 
     private boolean startFragment(String fragment, Bundle args, String key) {
@@ -268,8 +283,18 @@ public class SettingsActivity extends FragmentActivity
                         }
                     });
                     return true;
-            }
 
+                case KEY_ICON_PACK:
+                    ReloadingListPreference icons = (ReloadingListPreference) findPreference(KEY_ICON_PACK);
+                    icons.setOnReloadListener(new IconPackPrefSetter(mContext));
+                    icons.setOnPreferenceChangeListener((pref, val) -> {
+                        IconDatabase.clearAll(mContext);
+                        IconDatabase.setGlobal(mContext, (String) val);
+                        AppReloader.get(mContext).reload();
+                        return true;
+                    });
+                    return true;
+            }
             return true;
         }
 
